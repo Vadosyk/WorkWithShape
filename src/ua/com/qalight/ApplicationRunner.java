d81cf18ef9bcb95184ae6f3f085e778b51c56b5d
vadosyk <vadosyk@yahoo.com>
2019-02-27 01:17:46 +0200

    Work with Array

--- src/ua/com/qalight/ApplicationRunner.java
+++ src/ua/com/qalight/ApplicationRunner.java
@@ -1,11 +1,14 @@
 package ua.com.qalight;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import ua.com.qalight.Circle.Circle;
 import ua.com.qalight.Square.Square;
 import ua.com.qalight.Triangle.Triangle;
+import ua.com.qalight.abstraction.Shape;
 
 public class ApplicationRunner {
 	
@@ -26,6 +29,29 @@ public class ApplicationRunner {
 		logger.info("Triangle with bese " + triangle.getBese() + " and heigth " + triangle.getHeigth() + " has area " + triangle.getArea());
 		
 
+		List<Shape> shapes = new ArrayList();
+		
+		Shape square1 = new Square(10);
+		Shape square2 = new Square(100);
+		Shape square3 = new Square(1000);
+		Shape square4 = new Square(10000);
+		
+		Shape circle1 = new Circle(10);
+		Shape circle2 = new Circle(100);
+		Shape circle3 = new Circle(1000);
+		Shape circle4 = new Circle(10000);
+		
+		shapes.add(square1);
+		shapes.add(square2);
+		shapes.add(square3);
+		shapes.add(square4);
+		
+		for(int i = 0; i < shapes.size(); i++){
+			logger.info("Shape" + shapes.get(i).get.Class().getSimpleName() + "has area" + shapes.get(i).getArea());
+		}
+		
 	}
 
 }
+
+
